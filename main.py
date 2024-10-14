b789dec3b66445cec3ca50b41b97caadc45f19bf
robernaveiras <mephysto@me.com>
2024-10-14 21:34:48 +0200

    Added report and short

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    counted_characters = get_count_character(text) 
-    print(f"{num_words} words found in the document")
-    print(counted_characters)
+    chars_dict = get_chars_dict(text)
 
+    # Convert chars_dict to a list of dictionaries
+    char_list = [{"char": char, "num": count} for char, count in chars_dict.items()]
+    
+    # Sort the list
+    char_list.sort(reverse=True, key=sort_on)
+
+    # Print the report
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    
+    for char_dict in char_list:
+        if char_dict["char"].isalpha():
+            print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+    
+    print("--- End report ---")
+
+def sort_on(dict_item):
+    return dict_item["num"]
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_count_character(text):
-    counted_characerts = {}
-    for character in text:
-        lowered = character.lower()
-        if lowered in counted_characerts:
-            counted_characerts[lowered] += 1
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            counted_characerts[lowered] = 1
-    return counted_characerts
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -27,5 +44,3 @@ def get_book_text(path):
 
 
 main()
-
-
